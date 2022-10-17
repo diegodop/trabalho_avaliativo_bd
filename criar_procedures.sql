b6949f3a9e5dc9c3073fee89c3667187b0ddf4a3
Diego Oliveira Peixoto <f0fp1407@t-systems.com.br>
2022-10-17 16:47:10 -0300

    Push semifinal dois

--- criar_procedures.sql
+++ criar_procedures.sql
@@ -12,6 +12,19 @@ call pCadastrarMedico('NOVO MÉDICO');
 select * from medicos;
 
 DELIMITER $$
+create procedure pCadastrarExame(in nomeExame varchar(60))
+begin
+	declare msg varchar(100);
+	insert into exames(nome) values (nomeExame);
+	set msg = concat('Exame ',nomeExame,' cadastrado com sucesso!');
+    select msg;
+end$$;
+DELIMITER ;
+
+call pCadastrarExame('NOVO EXAME');
+select * from exames;
+
+DELIMITER $$
 create procedure pCadastrarEspecialidade(in descricaoEspecialidade varchar(60))
 begin
 	declare msg varchar(100);
@@ -72,3 +85,75 @@ DELIMITER ;
 
 call pAlterarEspecialidade('NOVA ESPECIALIDADE', 'VELHA ESPECIALIDADE');
 select * from especialidades;
+
+DELIMITER $$
+create procedure pAlterarPaciente(in nomeAtual varchar(60), in nomeNovo varchar(60))
+begin
+	declare qtd int;
+    declare msg varchar(100);
+    select count(id) into qtd from pacientes where nome = nomeAtual;
+	if qtd > 0 then
+		update pacientes set nome = nomeNovo where nome = nomeAtual;
+        set msg = 'Paciente alterado com sucesso!';
+	else
+		set msg = "O paciente informado não existe.";
+    end if;
+end$$;
+DELIMITER ;
+
+call pAlterarPaciente('NOVO PACIENTE', 'VELHO PACIENTE');
+select * from pacientes;
+
+DELIMITER $$
+create procedure pRemoverMedico(in nomeAtual varchar(60))
+begin
+	declare qtd int;
+    declare msg varchar(100);
+    select count(id) into qtd from medicos where nome = nomeAtual;
+	if qtd > 0 then
+		delete from medicos where nome = nomeAtual;
+        set msg = 'Médico removido com sucesso!';
+	else
+		set msg = "O médico informado não existe.";
+    end if;
+end$$;
+DELIMITER ;
+
+call pRemoverMedico('VELHO MÉDICO');
+select * from medicos;
+
+DELIMITER $$
+create procedure pRemoverEspecialidade(in descAtual varchar(60))
+begin
+	declare qtd int;
+    declare msg varchar(100);
+    select count(id) into qtd from especialidades where descricao = descAtual;
+	if qtd > 0 then
+		delete from especialidades where descricao = descAtual;
+        set msg = 'Especialidade removida com sucesso!';
+	else
+		set msg = "A especialidade informada não existe.";
+    end if;
+end$$;
+DELIMITER ;
+
+call pRemoverEspecialidade('VELHA ESPECIALIDADE');
+select * from especialidades;
+
+DELIMITER $$
+create procedure pRemoverPaciente(in nomeAtual varchar(60))
+begin
+	declare qtd int;
+    declare msg varchar(100);
+    select count(id) into qtd from pacientes where nome = nomeAtual;
+	if qtd > 0 then
+		delete from pacientes where nome = nomeAtual;
+        set msg = 'Paciente removido com sucesso!';
+	else
+		set msg = "O paciente informado não existe.";
+    end if;
+end$$;
+DELIMITER ;
+
+call pRemoverPaciente('VELHO PACIENTE');
+select * from pacientes;
